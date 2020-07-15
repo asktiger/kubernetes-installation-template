ec3e07a2f7e5d20f63c0f69cfe6359a970d4a86c
asktiger <ask_tiger@hotmail.com>
2020-07-15 10:51:30 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ sudo yum update -y
 sudo yum install -y docker-ce
 
 sudo systemctl enable docker && systemctl start docker
-sudo docker run --rm -v $(pwd):/k8s-deploy centos:7.6.1810 bash -c "cd /k8s-deploy; bash install-k8s.sh"
+sudo docker run --rm -v $(pwd):/k8s-deploy centos:7.8.2003 bash -c "cd /k8s-deploy; bash install-k8s.sh"
